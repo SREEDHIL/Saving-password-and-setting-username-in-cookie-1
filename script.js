2f3a1c49692b66f542d3da8b809cc28b29d08f5b
SREEDHIL <sreedhilpavishanker@gmail.com>
2025-03-28 09:52:08 +0530

    updated

--- script.js
+++ script.js
@@ -14,6 +14,18 @@ document.addEventListener('DOMContentLoaded', function() {
       name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
   }
 
+  let count = getCookie("count");
+
+  if (count){
+    count = parseInt(count) + 1;
+  }else{
+    count = 1;
+  }
+
+  setCookie("count", count, 7);
+
+  document.body.innerHTML = `<h1> Page Visit Count: ${count}</h1>`;
+
   // 1. Get the value of the 'count' cookie
   // 2. If the cookie exists, increment the value and update the cookie
   // 3. If the cookie does not exist, create it and set the value to 1
